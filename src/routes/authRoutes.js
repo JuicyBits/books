b822bf8505dc636903906e8958628dbe467c995c
Matt Anderson <clutchstar@gmail.com>
2017-10-31 19:44:30 -0600

    Push 3

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,16 +1,32 @@
 var express = require('express');
 var authRouter = express.Router();
-// var mongodb = require('mongodb').MongoClient;
+var mongodb = require('mongodb').MongoClient;
 
 var router = () => {
-    authRouter.route('/login').post((req, res) => {
-        if(!req.body.username){return res.send('no body');}
-        res.redirect('/');
-    })
-    .get((req, res) => {
-        console.log('test');
-        res.render('login');
+    authRouter.route('/login')
+    .post((req, res) => {
+            if(!req.body.userName || !req.body.passWord){
+                return res.sendStatus(403);
+            }
+            res.redirect('/');
     });
+    authRouter.route('/register')
+        .post((req, res) => {
+            if(!req.body.userName || !req.body.passWord){
+                return res.sendStatus(403);
+            }
+            var url = 'mongodb://localhost:27017/books'; //27017 is default Mongo port
+            mongodb.connect(url, (err, db) => {
+                var collection = db.collection('books');
+                collection.insert({
+                    user: req.body.userName,
+                    password: req.body.passWord
+                });
+                db.close();
+            });
+            res.redirect('/');
+        });
+
     return authRouter;
 }
 module.exports = router;
